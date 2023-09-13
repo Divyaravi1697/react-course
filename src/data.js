2f1d9b16d522d8b6c2654834f31af070f5b057dc
Divyaravi1697 <divyaa1697@gmail.com>
2023-09-13 14:16:55 +0530

    create components

---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const pageLinks = [
+    { id: 1, href: '#home', text: 'home' },
+    { id: 2, href: '#about', text: 'about' },
+    { id: 3, href: '#services', text: 'services' },
+    { id: 4, href: '#tours', text: 'tours' },
+
+];
+export const socialLinks = [
+    { id: 1, href: 'https://www.facebook.com', icon: 'fas fa-facebook' },
+    { id: 2, href: 'https://www.twitter.com', icon: 'fas fa-twitter' },
+
+    { id: 3, href: 'https://www.facebook.com', icon: 'fas fa-squarespace' },
+
+];
